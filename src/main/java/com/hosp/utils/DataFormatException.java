7f4f91b776363e44ba8b58d4b222dbb946c4d777
Mansi Punia <mansipunia97@gmail.com>
2025-11-05 10:48:09 +0530

    Added Validation and custom exceptions

---
+++ src/main/java/com/hosp/utils/DataFormatException.java
@@ -0,0 +1,7 @@
+package com.hosp.utils;
+
+public class DataFormatException extends AppException 
+{
+    public DataFormatException(String msg) { super(msg); }
+    public DataFormatException(String msg, Throwable cause) { super(msg, cause); }
+}
